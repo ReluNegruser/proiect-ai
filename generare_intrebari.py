f521eadc24d8c610f95849618fe6ef82d9aa03bf
Iordache Marian <iordache.marian@outlook.com>
2018-01-15 09:30:32 +0200

    Generare intrebari v2.

--- generare_intrebari.py
+++ generare_intrebari.py
@@ -1,21 +1,21 @@
 import os, random
 
 def random_line(fisier, no=1):
-	line = ''
+	line = []
 	while (no >= 1):
-		line = line + random.choice(open(fisier).readlines())
+		one_line = random.choice(open(fisier).readlines())
+		line.append(one_line.rstrip())
 		no = no - 1
 	return line
 
 
 def generare():
 	filename = random.choice(os.listdir("intrebari"))
-	os.chdir("intrebari")
 	sentence = random_line('intrebari.txt')
-	no = sentence.split()[0]
-	sentence = sentence.split(' ', 1)[1]
+	os.chdir("intrebari")
+	no = sentence[0].split()[0]
+	sentence = sentence[0].split(' ', 1)[1]
 	words = random_line(filename, int(no))
-
 	for i in range(0, int(no)):
 		sentence = sentence.replace("_", words[i], 1)
 
